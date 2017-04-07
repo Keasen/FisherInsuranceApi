b93be7b5d1c09b272eb7749b86f61af83d606178
Xudong Hao <xudonghao@Keasens-MBP.attlocal.net>
2017-04-06 21:59:52 -0400

    lab7

--- FisherInsuranceApi/Data/FisherContext.cs
+++ FisherInsuranceApi/Data/FisherContext.cs
@@ -1,15 +1,27 @@
-using Microsoft.EntityFrameworkCore; 
-using FisherInsuranceApi.Models; 
+using Microsoft.EntityFrameworkCore;
+using FisherInsuranceApi.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+
 namespace FisherInsuranceApi.Data 
 { 
     
-    public class FisherContext : IdentityDbContext<Applicationuser> {
+   public class FisherContext : IdentityDbContext<ApplicationUser> {
         public DbSet<Claim> Claims { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { 
             string connection = "User ID=keasen;Password=123;Host=localhost;Port=5433;Database=fisher-insurance;Pooling=true;"; 
             optionsBuilder.UseNpgsql(connection); 
+        }
+
+        internal void SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
         }
     }    
 }
