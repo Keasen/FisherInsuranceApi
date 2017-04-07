ce54f2a1320914a4fb22029157f68c6d4b9d3e97
Xudong Hao <xudonghao@Keasens-MBP.attlocal.net>
2017-04-07 01:05:04 -0400

    lab7

--- FisherInsuranceApi/wwwroot/app/components/login/login.component.ts
+++ FisherInsuranceApi/wwwroot/app/components/login/login.component.ts
@@ -1,3 +1,18 @@
+import { AuthService } from '../../auth.service';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+selector: 'login',
+templateUrl: './app/components/login/login.component.html',
+styleUrls: ['./app/components/login/login.component.css']
+})
+
 export class LoginComponent {
     title = "Login";
     loginForm = null;
